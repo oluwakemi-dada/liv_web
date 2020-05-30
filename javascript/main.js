790b0c648f578caba5c9683e852e07729a59b58d
Dada Oluwakemi <dadaoluwakemi97@gmail.com>
2020-05-30 13:39:48 -0700

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$("#main-nav a, .btn").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 60,
+      },
+      800
+    );
+  }
+});
